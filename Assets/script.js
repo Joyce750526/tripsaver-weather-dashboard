08bf1abfe828b850ea29b8cf3b4302b7ffef7853
Joyce Chen <joyceideas@outlook.com>
2022-08-15 23:33:50 -0400

    rewriting codes in JS

--- Assets/script.js
+++ Assets/script.js
@@ -59,80 +59,9 @@ function fetchCity(latitude, longitude) {
             var tempEl = document.createElement('p');
             tempEl.classList.add('card-text');
             
-            humidEl.textContent = `Humidity: ${data.main.humidity} %`;
-            tempEl.textContent = `Temperature: ${data.main.temp} °F`;
-            var cardBody = document.createElement('div');
-            cardBody.classList.add('card-body');       
-            cardBody.appendChild(titleEl);
-            cardBody.appendChild(tempEl);
-            cardBody.appendChild(humidEl);
-            cardBody.appendChild(windEl);
-            cardEl.appendChild(cardBody);
-            todayEl.appendChild(cardEl);
 
-            // todayEl = document.querySelector("#current-day");
-            // todayEl.textContent = "";
-            
-            // var titleEl=document.createElement("h4");
-            // titleEl.classList.add("card");
-       
 
         });
 }
-
-//   function printResults(resultObj) {
-//     console.log(resultObj);
-
-//     // set up `<div>` to hold result content
-//     var resultCard = document.createElement('div');
-//     resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
-
-//     var resultBody = document.createElement('div');
-//     resultBody.classList.add('card-body');
-//     resultCard.append(resultBody);
-
-//     var titleEl = document.createElement('h3');
-//     titleEl.textContent = resultObj.title;
-
-//     var bodyContentEl = document.createElement('p');
-//     bodyContentEl.innerHTML =
-//       '<strong>Date:</strong> ' + resultObj.date + '<br/>';
-
-//     if (resultObj.subject) {
-//       bodyContentEl.innerHTML +=
-//         '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-//     } else {
-//       bodyContentEl.innerHTML +=
-//         '<strong>Subjects:</strong> No subject for this entry.';
-//     }
-
-//     if (resultObj.description) {
-//       bodyContentEl.innerHTML +=
-//         '<strong>Description:</strong> ' + resultObj.description[0];
-//     } else {
-//       bodyContentEl.innerHTML +=
-//         '<strong>Description:</strong>  No description for this entry.';
-//     }
-
-//     var linkButtonEl = document.createElement('a');
-//     linkButtonEl.textContent = 'Read More';
-//     linkButtonEl.setAttribute('href', resultObj.url);
-//     linkButtonEl.classList.add('btn', 'btn-dark');
-
-//     resultBody.append(titleEl, bodyContentEl, linkButtonEl);
-
-//     resultContentEl.append(resultCard);
-//   }
-
-
-
-// function handleUserInput(event) {
-//     event.preventDefault();
-//     var searchInput = document.querySelector(".search-bar");
-//     var cityName = searchInput.value;
-//     console.log(cityName);
-//     fetchCity(cityName);
-//   }
-
 
 searchBtn.addEventListener("click", handleUserInput);
